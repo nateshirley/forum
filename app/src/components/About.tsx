c0997f2323550b1d1a7903b3c0b6a2ceca4d4acd
Nathan Shirley <nshirley22@gmail.com>
2021-12-15 10:22:56 -0500

    rewording on about

--- app/src/components/About.tsx
+++ app/src/components/About.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 
 function About() {
-
-
     return (
         <div className="component-parent">
             prh
@@ -11,21 +9,21 @@ function About() {
             <br />
             <br />
             <div className="about-div">
-                a ParisRadioHour is a set of ideas about music/crypto.
+                Paris Radio Hour is a music forum on solana.
                 <br />
                 <br />
-                each week, a new PRH is written on chain—by the community—and then auctioned off by the protocol
+                each week, a new PRH is created on chain—by the community—and then auctioned off by the protocol.
                 <br />
                 <br />
                 auction proceeds enter the community's treasury, where they are used to further the PRH
                 <br />
                 <br />
-                every PRH is created, issued, and auctioned permissionlessly. all data comes and goes directly from the solana blockchain
+                every PRH is created, issued, and auctioned permissionlessly. all forum state comes and goes directly from the solana blockchain
                 <br />
                 <br />
                 ---
                 <br />
-                p.s. community members can submit one post and one like per week. PRH is currently in beta on the solana devnet. try it <Link to="/">here</Link>
+                p.s. community members can submit one post and one like per week. the top 10 posts from each week are included in the historical PRH. we are currently in beta on the solana devnet. try it <Link to="/">here</Link>
             </div>
 
         </div>
