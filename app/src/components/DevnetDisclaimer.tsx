383090e80b576a619435170f220d693436f04e8a
Nathan Shirley <nshirley22@gmail.com>
2021-12-15 21:36:13 -0500

    including about button in disclaimer

--- app/src/components/DevnetDisclaimer.tsx
+++ app/src/components/DevnetDisclaimer.tsx
@@ -20,25 +20,31 @@ function DevnetDisclaimer() {
     const showMeHow = () => {
         history.push("/networkchange")
     }
+    const whatIsThis = () => {
+        history.push("/about")
+    }
 
     const content = () => {
         if (awareness === AWARENESS.default) {
             return (
                 <div>
-                    PRH is in beta on the solana devnet. all transactions on this internet site are executed via devnet aka they do not spend real sol.
+                    PRH is in beta on the solana devnet. all transactions on this internet site are executed via devnet aka they do not spend real sol.&nbsp;
                     <button onClick={didClickLearnMore} className="disclaimer-learn-more">learn more</button>
                 </div>
             )
         } else if (awareness === AWARENESS.learnMore) {
             return (
                 <div>
-                    PRH is in beta on the solana devnet. all transactions on this internet site are executed via the solana devnet aka they do not spend real sol.
+                    PRH is in beta on the solana devnet. all transactions on this internet site are executed via devnet aka they do not spend real sol.
                     <br />
                     <br />
                     if u want devnet sol, u can click the airdrop button at the bottom of the page.
                     <br />
                     <br />
-                    to see your membership NFT, you'll have to switch your phantom wallet to devnet.<button onClick={showMeHow} className="disclaimer-learn-more">show me how</button>
+                    to see your membership NFT, you'll have to switch your phantom wallet to devnet.&nbsp;<button onClick={showMeHow} className="disclaimer-learn-more">show me how</button>
+                    <br />
+                    <br />
+                    <button onClick={whatIsThis} className="disclaimer-learn-more">what is PRH?</button>
                 </div>
             )
         }
